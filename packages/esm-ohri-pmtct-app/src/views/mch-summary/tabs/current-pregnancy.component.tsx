f1dda08fd23e60d8698046f584adad39a03976e7
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-01-16 21:49:17 +0300

    OHRI-1212 Mother HIV status and EDD populated in the summary

--- packages/esm-ohri-pmtct-app/src/views/mch-summary/tabs/current-pregnancy.component.tsx
+++ packages/esm-ohri-pmtct-app/src/views/mch-summary/tabs/current-pregnancy.component.tsx
@@ -136,26 +136,16 @@ const CurrentPregnancy: React.FC<PatientChartProps> = ({ patientUuid }) => {
           }
           return artInitiation;
         },
-        hasSummary: true,
         getSummaryObsValue: (encounter) => {
-          if (getObsFromEncounter(encounter, hivTestResultConcept) === '--') {
-            return '--';
-          } else {
-            return getObsFromEncounter(encounter, visitDate, true);
-          }
+          return getObsFromEncounter(encounter, visitDate, true);
         },
       },
       {
         key: 'expectedDeliveryDate',
         header: t('expectedDeliveryDate', 'Expected Delivery Date'),
         encounterUuid: antenatalEncounterType,
-        hasSummary: true,
         getObsValue: (encounter) => {
           return getObsFromEncounter(encounter, eDDConcept, true);
-        },
-        getSummaryObsValue: (encounter) => {
-          let edd = getObsFromEncounter(encounter, eDDConcept, true);
-          return edd === '--' ? edd : `In ${calculateDateDifferenceInDate(edd)}`;
         },
       },
       {
